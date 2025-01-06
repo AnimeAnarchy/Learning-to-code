ac95308ca4aa9bb4612a8440b5b11d6b277845b7
AnimeAniki <77264247+AnimeAnarchy@users.noreply.github.com>
2025-01-06 17:39:00 -0500

    Started on Section 2

---
+++ C#/1-Notes/2) Variables and DataTypes/variableExample.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 1-Notes.2) Variables and DataTypes
+{
+    public class variableExample
+    {
+        int a; // "2) Integers" example
+        
+    }
+}
